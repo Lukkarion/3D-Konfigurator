c57923e3ae5c2d6101b24dacc50ca5aa88e04d0f
s72528 <s72528@beuth-hochschule.de>
2022-07-05 18:45:31 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const fs = require('fs');
+const extract = require('extract-zip')
+const formidable = require('formidable');
+const path = require('path');
+const fileUpload = require("express-fileupload");
+
+const uploadDir = path.join(__dirname, '/public/obj/');
+const extractDir = path.join(__dirname, '/public/obj/');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+if (!fs.existsSync(extractDir)) {
+  fs.mkdirSync(extractDir);
+}
+
+const server = express();
+
+const extractZip = (file, destination, deleteSource) => {
+  extract(file, { dir: destination }, (err) => {
+    if (!err) {
+      if (deleteSource) fs.unlink(file);
+      nestedExtract(destination, extractZip);
+      console.error(file);
+    } else {
+      console.error(err);
+    }
+  }
+  );
+};
+
+const nestedExtract = (dir, zipExtractor) => {
+  fs.readdirSync(dir).forEach((file) => {
+    if (fs.statSync(path.join(dir, file)).isFile()) {
+      if (path.extname(file) === '.zip') {
+        zipExtractor(path.join(dir, file), dir, true);
+      }
+    } else {
+      nestedExtract(path.join(dir, file), zipExtractor);
+    }
+  });
+};
+
+const getDirectories = (path) => {
+    return fs.readdirSync(path).filter(function (file) {
+      return fs.statSync(path+'/'+file).isDirectory();
+    });
+}
+
+server.use(express.static('public'));
+server.use(fileUpload());
+
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+server.get("/models", (req, res) => {
+  return res.status(200).send(getDirectories(__dirname + "/public/obj/"));
+});
+
+server.post("/upload", (req, res) => {
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  const file = req.files.myFile;
+  const extensionName = path.extname(file.name);
+  const allowedExtension = ['.zip'];
+
+  if(!allowedExtension.includes(extensionName)){
+      return res.status(422).send("Invalid File Format (only .zip allowed)");
+  }
+
+  const dirPath = __dirname + "/public/obj/" + file.name;
+
+  file.mv(dirPath, (err) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.redirect('/');
+  });
+
+  extractZip(dirPath, dirPath + "_extract", true);
+});
+
+server.listen(8080, (err) => {
+  if (err) throw err;
+});
---
+++ public/js/script.js
@@ -0,0 +1,152 @@
+import * as THREE from './three.module.js';
+import { GLTFLoader } from './GLTFLoader.js';
+import { OrbitControls } from './OrbitControls.js';
+var scene, renderer, camera;
+
+//---------------------------------------------------
+// Scene
+
+scene = new THREE.Scene();
+scene.background = new THREE.Color(0xf1f1f1);
+scene.fog = new THREE.Fog(0xf1f1f1, 20, 100);
+
+//---------------------------------------------------
+// Camera
+
+camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight);
+camera.position.set(0, 0, 2.75);
+
+//---------------------------------------------------
+// Renderer
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//---------------------------------------------------
+// Controls
+
+var controls = new OrbitControls(camera, renderer.domElement);
+var mouse = new THREE.Vector2();
+controls.update();
+
+//---------------------------------------------------
+// Light
+
+var abint = new THREE.AmbientLight(0xffffff, 0.7);
+var spt = new THREE.SpotLight(0xffffff, 0.3, 0, 0.1, 1);
+spt.position.set(50, 2500, 2000);
+scene.add(abint);
+scene.add(spt);
+
+//---------------------------------------------------
+// 3D Model Loader
+
+var loader = new GLTFLoader();
+document.getElementById('js-next').addEventListener("click", nextModel);
+
+var i = 0;
+var models = JSON.parse(httpGet('/models'));
+var obj = scene.getObjectByName(models[i]);
+function nextModel() {
+  if (obj !== undefined) {
+    scene.remove(obj);
+  }
+  var dest = `../obj/${models[i++]}/scene.gltf`;
+  loader.load(dest, function( gltf ){
+    obj = gltf.scene;
+    obj.name = models[i];
+    scene.add(gltf.scene);
+  });
+
+  if (i === models.length) {
+    i = 0;
+  }
+}
+
+function httpGet(theUrl) {
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false );
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
+
+nextModel();
+
+//---------------------------------------------------
+// select clicked Part of the model
+
+var raycaster = new THREE.Raycaster();
+var selected = null;
+
+renderer.domElement.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+
+  mouse.x = event.clientX / window.innerWidth * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(mouse, camera);
+  var intersects = raycaster.intersectObjects(scene.children, true);
+  if (intersects.length > 0) {
+    selected = intersects[0].object;
+  }
+}
+
+//---------------------------------------------------
+//create the color picker elements
+
+const colors = [
+  {color: '66ff00'}, //green
+  {color: '1974d2'}, //dark blue
+  {color: '08e8de'}, //light blue
+  {color: 'fff000'}, //yellow
+  {color: 'ff007f'}  //magenta
+]
+
+const TRAY = document.getElementById('js-slide');
+
+function buildColors(colors) {
+  for (let [i, color] of colors.entries()) {
+    let picker = document.createElement('div');
+    picker.classList.add('tray__picker');
+      picker.style.background = "#" + color.color;
+    picker.setAttribute('data-key', i);
+    TRAY.append(picker);
+  }
+}
+buildColors(colors);
+
+// functions for the color picker elements
+
+const pickeres = document.querySelectorAll(".tray__picker");
+
+for (const picker of pickeres) {
+  picker.addEventListener('click', selectPicker);
+}
+
+function selectPicker(e) {
+     let color = colors[parseInt(e.target.dataset.key)];
+     let newColor = new THREE.Color( parseInt('0x' + color.color));
+     setMaterial(obj, selected.name, newColor);
+}
+
+function setMaterial(parent, type, clr) {
+  parent.traverse((obj) => {
+   if (obj.name != null) {
+     if (obj.name == type) {
+          obj.material.color = clr;
+       }
+   }
+ });
+}
+
+//---------------------------------------------------
+//rendering
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
